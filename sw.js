d2147fccd17e8902a4719d0ec8e17df9114edb06
sebacarterp-creator <seba.carter.p@gmail.com>
2026-08-08 04:43:20 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,23 @@
-const CACHE = "epp-cache-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-];
+const CACHE = "epp-cache-v2";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener("fetch", (e) => {
+  if (e.request.url.includes("supabase.co")) return; // nunca cachear llamadas a la base de datos
   e.respondWith(
-    caches.match(e.request).then((cached) => {
-      return (
-        cached ||
-        fetch(e.request)
-          .then((res) => {
-            const resClone = res.clone();
-            caches.open(CACHE).then((cache) => cache.put(e.request, resClone));
-            return res;
-          })
-          .catch(() => cached)
-      );
-    })
+    caches.match(e.request).then((cached) =>
+      cached || fetch(e.request).then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, clone));
+        return res;
+      }).catch(() => cached)
+    )
   );
 });
